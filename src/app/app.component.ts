0d763bced914cad75e0c0c4d2a5453d8a275075f
Bolaji Olajide <bolaji.olajide@andela.com>
2017-04-12 19:51:55 +0100

    Implement routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <div><h1> {{ pageTitle }} </h1>
-  <app-product> Loading ... </app-product>
-  </div>`
-  // templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
+  // template: `
+  // <div>
+  //   <nav class="navbar navbar-default">
+  //     <div class="container-fluid">
+  //       <a class="navbar-brand"> {{ pageTitle }} </a>
+  //       <ul class='nav navbar-nav'> 
+  //         <li><a [routerLink]="['/welcome']"> Home </a></li>
+  //         <li><a [routerLink]="['/product']"> Product List </a></li>
+  //       </ul>
+  //     </div>
+  //   </nav>
+  // </div>
+  // `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   pageTitle: string = 'Acme Product Management';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { productFilterPipe } from './product/product-filter.pipe';
 import { StarComponent } from './star/star.component'
+import { ProductService } from "app/product.service";
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+  { path: 'product', component: ProductComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    // Import the pipe so as to make it available to all components within this module.
     productFilterPipe,
     StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
